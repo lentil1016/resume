5ec66cc0373c4c0287cde286cdda7fd523eaa5f5
Lentil1016 <lentil1016@gmail.com>
2018-10-13 16:44:03 +0800

    refactor update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 pushd /var/www/html/resume.lentil1016.cn/user
-	rm -rf pages config
 	git clone https://github.com/Lentil1016/resume.git
+	rm -rf pages config
 	mv resume/pages pages
 	mv resume/config config
 	rm -rf resume
